3412637a62921fa799e455ac31eb1c9a17aeb115
Thenu <thenusandul@gmail.com>
2022-04-08 00:02:29 +0530

    downgraded

--- js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 03, 17, 8)}); // year, month, date, hour
-        });
-});		
-
